227d2e13812697747fc05b9852bc5728614ef5ec
YuvrajChauhan388 <yuvrajchauhan993@gmail.com>
2025-03-18 10:58:04 +0530

    Update wsgi_dev.py

--- wsgi_dev.py
+++ wsgi_dev.py
@@ -2,5 +2,6 @@
 This module is the entry point for the WSGI application."""
 
 from app import app
+
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8051)
+    app.run_server(debug=False, host='0.0.0.0', port=8051)
