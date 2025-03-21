fc6c1623394eb6282e9ac95ede766a4dde307553
Yuvraj Chauhan <yuvrajchauhan993@gmail.com>
2025-03-21 15:14:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -175,4 +175,7 @@ with container:
         st.download_button("Download Raw Signal Stats", data=csv, file_name="raw_signal_stats.csv", mime='text/csv')
 
         noise = Signal - denoised_signal
-        stats = calculate_statistical_data
+        stats = calculate_statistical_data(denoised_signal, noise)
+        df_stats_denoised = pd.DataFrame(stats.items(), columns=["Parameter", "Value"])
+        csv_denoised = convert_df(df_stats_denoised)
+        st.download_button("Download Denoised Signal Stats", data=csv_denoised, file_name="denoised_signal_stats.csv", mime='text/csv')
